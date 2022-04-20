aec43b9f77054bb2456c2b2dd1255e672e192762
App Academy Student <email@email.com>
2022-04-20 11:46:46 -0400

    did reducers

---
+++ benchbnb/frontend/reducers/session_reducer.js
@@ -0,0 +1,17 @@
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from "../actions/session_actions";
+
+const null_state = Object.freeze({id:null});
+
+const sessionReducer = (state = null_state , action) => {
+    Object.freeze(state);
+    switch(action.type){
+        case RECEIVE_CURRENT_USER:
+            return {id:action.currentUser.id};
+        case LOGOUT_CURRENT_USER:
+            return null_state;
+        default:
+            return state;
+    }
+};
+
+export default sessionReducer;
